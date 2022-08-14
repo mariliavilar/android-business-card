6d666db4b2172d7bae370c0f3ec4a6c96ce09429
Marilia Vilar <marilia@roadmaps.com.br>
2022-08-14 20:20:54 -0300

    🎨 Estrutura de Pastas do Projeto

--- app/src/main/java/com/mariliavilar/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/mariliavilar/businesscard/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mariliavilar.businesscard
+package com.mariliavilar.businesscard.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mariliavilar.businesscard.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
