2612bc9884cfa37a7428aec7c2c9e5a68ddff213
Marilia Vilar <marilia@roadmaps.com.br>
2022-08-14 20:45:08 -0300

    🔧 ✨ Add Binding

--- app/src/main/java/com/mariliavilar/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/mariliavilar/businesscard/ui/MainActivity.kt
@@ -2,11 +2,17 @@ package com.mariliavilar.businesscard.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.mariliavilar.businesscard.R
+import com.mariliavilar.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // [BINDING#2]
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // [BINDING#3]
+        setContentView(binding.root)
     }
 }
